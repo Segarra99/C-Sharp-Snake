ea397b36f85bbbd3f830fe00c287cb206f3c3c47
Manuel Segarra <manuel.gs.segarra@gmail.com>
2023-11-13 14:31:15 +0000

    score working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -104,6 +104,7 @@ namespace Snake
         private void Draw()
         {
             DrawGrid();
+            ScoreText.Text = $"SCORE {gameState.Score}";
         }
 
         private void DrawGrid()
