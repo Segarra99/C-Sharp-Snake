6feafc6189b931939afd1d2f008d1ba95fe4464f
Manuel Segarra <manuel.gs.segarra@gmail.com>
2023-11-13 14:07:46 +0000

    gamestate drawn

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,13 +20,32 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            { GridValue.Empty, Images.Empty },
+            { GridValue.Snake, Images.Body },
+            { GridValue.Food, Images.Food }
+        };
+
         private readonly int rows = 15, cols = 15;
         private readonly Image[,] gridImages;
+        private GameState gameState;
 
         public MainWindow()
         {
             InitializeComponent();
             gridImages = SetupGrid();
+            gameState = new GameState(rows, cols);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Draw();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
         }
 
         private Image[,] SetupGrid()
@@ -50,6 +69,23 @@ namespace Snake
             }
 
             return images;
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            for (int r= 0; r < rows; r++)
+            {
+                for (int c= 0; c < cols; c++)
+                {
+                    GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValToImage[gridVal];
+                }
+            }
         }
     }
 }
