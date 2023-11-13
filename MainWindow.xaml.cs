84702a4f7ceda087862ddd971e8d6641850d67fa
Manuel Segarra <manuel.gs.segarra@gmail.com>
2023-11-13 13:56:03 +0000

    added grid images

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages;
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = SetupGrid();
+        }
+
+        private Image[,] SetupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            return images;
         }
     }
 }
